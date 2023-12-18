ebb106a83cddc3f5e536d24918140f6c54fe7008
scfung <stanley.fung91@myhunter.cuny.edu>
2023-12-18 00:34:07 -0500

    fixing User.js again

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const Tasks = require('/taskModel');
+const Tasks = require('/tasksModel');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  tasks: taskSchema// Adding tasks array to user schema
+  tasks: taskSchema, // Adding tasks array to user schema
 });
 
 // Password hashing middleware
